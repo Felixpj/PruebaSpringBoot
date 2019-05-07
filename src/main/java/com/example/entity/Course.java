141894f16e96af38566f984a437c5bcf89e846db
Félix <felixpj@iesgalileo.es>
2019-05-07 13:07:15 +0200

    creada base datos, configurada persistencia , creada la primera entidad y repositorio JPA

---
+++ src/main/java/com/example/entity/Course.java
@@ -0,0 +1,63 @@
+package com.example.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="course")
+public class Course {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="hours")
+	private int hours;
+	
+	public Course() {}
+	
+	public Course(int id, String name, String description, int hours) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.hours = hours;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getHours() {
+		return hours;
+	}
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+	
+	
+	
+}
