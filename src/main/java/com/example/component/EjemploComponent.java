46dff2209c9af8670d6037a309b4c87ccaf171bb
Félix <felixpj@iesgalileo.es>
2019-05-07 09:12:58 +0200

    primer componente

---
+++ src/main/java/com/example/component/EjemploComponent.java
@@ -0,0 +1,17 @@
+package com.example.component;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
+
+@Component("ejemploComponent")
+public class EjemploComponent {
+		
+		private static final Log LOG=LogFactory.getLog(EjemploComponent.class);
+	
+		public void sayHello() {
+			LOG.info("HELLO FROM EJEMPLO_COMPONENT");
+		}
+	
+	
+}
--- src/main/java/com/example/controller/EjemploController.java
+++ src/main/java/com/example/controller/EjemploController.java
@@ -3,23 +3,31 @@ package com.example.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.component.EjemploComponent;
 import com.example.model.Person;
 
 @Controller
 @RequestMapping("/ejemplo")
-public class HelloWorldController {
+public class EjemploController {
 	
 	public static final String EXAMPLE_VIEW="ejemplo";
+	
+	@Autowired
+	@Qualifier("ejemploComponent")
+	private EjemploComponent ejemploComponent;
 	
 	//forma1
 	@GetMapping("/helloWorld")
 	public String helloWorld(Model model){
+		ejemploComponent.sayHello();
 		model.addAttribute("personas",getPersonas());
 		return EXAMPLE_VIEW;
 	}
